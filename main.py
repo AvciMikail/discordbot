b9936279f8a0672f546b9f6f446d6ffaa2d962fd
Mikail Avci Pxl <11902122@student.pxl.be>
2022-08-24 20:10:03 +0200

    test

---
+++ main.py
@@ -0,0 +1,231 @@
+from multiprocessing import Process
+import requests
+import time
+
+
+def bot1():
+    channel = 979787382566043709
+    headers = {"authorization": "OTM3NDY3OTA2ODI3MTEyNTY4.GGIPNX.Gdn6UDERHt4ijmFCBmTGiJX1ToCM4Wpu5vKABU"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers,
+                          json={"content": line})
+            time.sleep(1.8)
+
+
+# ashe
+
+
+def bot2():
+    channel = 979787382566043709
+    headers2 = {"authorization": "OTgwNzI4MjY3OTgyMTM5Mzky.GxAFHD.H_aflLSU7NzP5SK6JcJD4cq6TWBOSqkXiTbcP0"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers2,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama11
+
+
+def bot3():
+    channel = 979787382566043709
+    headers3 = {"authorization": "OTc5OTAxNjc3Nzk0NjU2Mjk2.GghRpA.L58b_r-KGUkeN7G7bU11VrXDPPA1YJJptyfMT4"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers3,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama4
+
+
+def bot4():
+    channel = 979787382566043709
+    headers4 = {"authorization": "OTA0NDI5MDY0NDIzNjEyNDQ3.GjxFxT.IYiTonnFadzuwqAdDfKO-AGdzp9CgH4Kva-zJs"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers4,
+                          json={"content": line})
+            time.sleep(1.8)
+
+            # car
+def bot5():
+    channel = 979787382566043709
+    headers5 = {"authorization": "OTc5ODk5MDMwODQwNzA1MDM1.Gfj37B.7ICrpHqkwFI3SwqJGPbfWKv7BEFOQTRZO5PBQs"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers5,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama2
+
+
+def bot6():
+    channel = 979787382566043709
+    headers6 = {"authorization": "OTc5ODk4NjExNjMyNTc4NjMw.G7WzeF.tWX1FhRZLsnnzFgfrCdAs_qJCe57nYEd4dHmsw"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers6,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama1
+
+
+def bot7():
+    channel = 979787382566043709
+    headers7 = {"authorization": "OTMzMTA1MzY3NDI3MTk0OTYw.G5VPLe.-zWVUrV_jUX6MrZTVQ4PUxtz7VG2byQye_jGBo"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers7,
+                          json={"content": line})
+            time.sleep(1.8)
+            # uwuwarm
+
+
+def bot8():
+    channel = 979787382566043709
+    headers8 = {"authorization": "OTc5OTAxMjU4OTkzMzg5NjI4.GV_-0B.vd-BVapStF1KN8D4WsniXBT6dW34H2UfZZqtpA"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers8,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama3
+
+
+def bot9():
+    channel = 979787382566043709
+    headers9 = {"authorization": "OTgwMjU1NzYxMjA2NzY3NzA3.GYZu42.TygOOsZwluT4gBfYXIjv5X0j4f9jadWN4RK8pU"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers9,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama5
+
+
+def bot10():
+    channel = 979787382566043709
+    headers10 = {"authorization": "OTgwMjU3NDcyODIyNTM4MjQx.G4MAzi.fBRf1L5AIvzsOzlGJIqI4vCZYstrtcReyu95Bs"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers10,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama6
+
+
+def bot11():
+    channel = 979787382566043709
+    headers11 = {"authorization": "OTgwNzE4MTExMjYxMDczNDU4.GB1W_0.SxteB0D7BV-lrljTicugK1yTG1CGouQK7IYFB0"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers11,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama7
+
+
+def bot12():
+    channel = 979787382566043709
+    headers12 = {"authorization": "OTgwNzIzMjM1MTk5MDA0NzMy.G8DEA7.wTA5IN7Zws1LnxMCZyKHbgu73r4fjrAbT8uUFg"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers12,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama8
+
+
+def bot13():
+    channel = 979787382566043709
+    headers13 = {"authorization": "OTgwNzI1OTMzMTY0MDc3MDg2.Gn4Reo.JtIP1wqeLk_Q86XRM8sud17D7sOms9BLkg4_mk"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers13,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama9
+
+
+def bot14():
+    channel = 979787382566043709
+    headers14 = {"authorization": "OTgwNzI3MjM5NDA0NTc2Nzk4.GOgZNA.1-bH4lPLHsGTTV8puNnOyGXxXEOK_jOvyt0jY8"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers14,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama10
+
+
+def bot15():
+    channel = 979787382566043709
+    headers15 = {"authorization": "OTgwNzI4MjY3OTgyMTM5Mzky.GxAFHD.H_aflLSU7NzP5SK6JcJD4cq6TWBOSqkXiTbcP0"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers15,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama11
+
+
+def bot16():
+    channel = 979787382566043709
+    headers16 = {"authorization": "OTgwNzI4OTc2NjU1OTIxMTgy.GWbYcH.bKF-rIxMfjVrKDhPh5etr_xu6jHyagokHiYUN8"}
+    lines = "o"
+    while True:
+        for line in lines:
+            requests.post(f"https://discord.com/api/v9/channels/{channel}/messages", headers=headers16,
+                          json={"content": line})
+            time.sleep(1.8)
+            # kaiosama11
+
+
+if _name_ == '_main_':
+    p1 = Process(target=bot1)
+    p1.start()
+    p2 = Process(target=bot2)
+    p2.start()
+    p3 = Process(target=bot3)
+    p3.start()
+    p4 = Process(target=bot4)
+    p4.start()
+    p5 = Process(target=bot5)
+    p5.start()
+    p6 = Process(target=bot6)
+    p6.start()
+    p7 = Process(target=bot7)
+    p7.start()
+    p8 = Process(target=bot8)
+    p8.start()
+    p9 = Process(target=bot9)
+    p9.start()
+    p10 = Process(target=bot10)
+    p10 .start()
+    p11= Process(target=bot11)
+    p11.start()
+    p12= Process(target=bot12)
+    p12.start()
+    p13 = Process(target=bot13)
+    p13.start()
+    p14 = Process(target=bot14)
+    p14.start()
+    p15 = Process(target=bot15)
+    p15.start()
+    p16 = Process(target=bot16)
+    p16.start()
